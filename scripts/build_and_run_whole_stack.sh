0065981a6e287e661095c62b8a72810b185ce1c0
Dušan Reljić <reljicd@gmail.com>
2018-03-20 12:08:41 +0100

    added util script for running both spring-boot-cassandra and Cassandra util scripts

---
+++ scripts/build_and_run_whole_stack.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+echo -e "\nExecute Cassandra Docker running script...\n"
+scripts/run_docker_cassandra.sh
+
+echo -e "\nExecute spring-boot-cassandra Docker running script...\n"
+scripts/run_docker.sh.sh
