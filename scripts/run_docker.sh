727c03e4b6858956b9ca1695d326df73b7df762e
Dušan Reljić <reljicd@gmail.com>
2018-03-20 12:08:00 +0100

    added util script for configuration, build and running of spring-boot-cassandra Docker containers

---
+++ scripts/run_docker.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+CONTAINER_NAME=spring-boot-cassandra
+echo -e "\nSet docker container name as ${CONTAINER_NAME}\n"
+IMAGE_NAME=${CONTAINER_NAME}:dev
+echo -e "\nSet docker image name as ${IMAGE_NAME}\n"
+PORT=9003
+echo -e "Set docker image PORT to ${PORT}\n"
+DOCKER_FOR_MAC_LOCALHOST=docker.for.mac.localhost
+
+echo -e "Maven package...\n"
+mvn package
+
+echo -e "\nStop running Docker containers with image tag ${CONTAINER_NAME}...\n"
+docker stop $(docker ps -a | grep ${CONTAINER_NAME} | awk '{print $1}')
+
+echo -e "\nDocker build image with name ${IMAGE_NAME}...\n"
+docker build -t ${IMAGE_NAME} -f docker/spring-boot-cassandra/Dockerfile .
+
+echo -e "\nStart Docker container of the image ${IMAGE_NAME} with name ${CONTAINER_NAME}...\n"
+docker run --rm -i -p ${PORT}:${PORT} \
+    -e SPRING_DATA_CASSANDRA_CONTACT_POINTS=${DOCKER_FOR_MAC_LOCALHOST} \
+    --name ${CONTAINER_NAME} \
+    ${IMAGE_NAME}
