472c568c6659dec9ae77262d228abe943cd86433
Dušan Reljić <reljicd@gmail.com>
2018-03-21 10:38:08 +0100

    added ExampleTableRepositoryLocalhostTest

---
+++ src/test/java/com/reljicd/repository/ExampleTableRepositoryLocalhostTest.java
@@ -0,0 +1,13 @@
+package com.reljicd.repository;
+
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Ignore
+public class ExampleTableRepositoryLocalhostTest extends ExampleTableRepositoryBaseTest {
+
+}
