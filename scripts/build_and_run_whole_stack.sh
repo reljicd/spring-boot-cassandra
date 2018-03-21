292074768cb52fe49e39e379c650c300ef93f49c
Dušan Reljić <reljicd@gmail.com>
2018-03-21 10:41:07 +0100

    small fixes in build_and_run_whole_stack.sh

--- scripts/build_and_run_whole_stack.sh
+++ scripts/build_and_run_whole_stack.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+export SPRING_DATA_CASSANDRA_CONTACT_POINTS=docker.for.mac.localhost
+
 echo -e "\nExecute Cassandra Docker running script...\n"
 scripts/run_docker_cassandra.sh
 
 echo -e "\nExecute spring-boot-cassandra Docker running script...\n"
-scripts/run_docker.sh.sh
+scripts/run_docker.sh
